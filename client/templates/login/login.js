5bfd333d9ab4cdc9a56a513c7b7041c4f7969c96
likestrike <sesim5351@gmail.com>
2017-08-24 18:45:54 +0900

    2017-08-24 essim session plugin 추가 login 기능 확인 (현재는 이메일 기준으로 가능함.) account 등록 기능. (필요 없지만 일단 추후를 생각해서 미리 구현해 놓는다. ) Goole sign 도 가능 auto run을 통한 처리.

--- client/templates/login/login.js
+++ client/templates/login/login.js
@@ -1,10 +1,25 @@
-// Accounts.ui.config({
-//   passwordSignupFields: 'USERNAME_ONLY' //  One of 'USERNAME_AND_EMAIL', 'USERNAME_AND_OPTIONAL_EMAIL', 'USERNAME_ONLY', or 'EMAIL_ONLY' (default).
-// });
+Accounts.ui.config({
+  passwordSignupFields: 'USERNAME_AND_EMAIL' //  One of 'USERNAME_AND_EMAIL', 'USERNAME_AND_OPTIONAL_EMAIL', 'USERNAME_ONLY', or 'EMAIL_ONLY' (default).
+});
 // google login 의 계정을 locus.com 으로 제한 한다. config
 Accounts.config({ restrictCreationByEmailDomain: 'locus.com' });
 
 
+
+Template.login.onCreated(function () {
+	this.autorun(function() {
+		// Whenever this session variable changes, run this function.
+		var message = Session.get('displayMessage');
+		if (message) {
+  			var stringArray = message.split('&amp;');
+  			// ui.notify(stringArray[0], stringArray[1])
+    	// 		.effect('slide')
+    	// 		.closable();
+    		console.log(stringArray[0]);
+  			Session.set('displayMessage', null);
+		}
+	});
+});
 Template.login.events({
 	'click #sign-google': function (e, t) {
 		Meteor.loginWithGoogle({
@@ -16,5 +31,25 @@ Template.login.events({
 		    FlowRouter.go('/');
 		  }
 		});
-	}
+	},
+	'submit #login-form' : function(e, t){
+      e.preventDefault();
+      // retrieve the input field values
+      var email = t.find('#login-email').value
+        , password = t.find('#login-password').value;
+        // Trim and validate your fields here.... 
+
+        // If validation passes, supply the appropriate fields to the
+
+        Meteor.loginWithPassword(email, password, function(err){
+        if (err){
+        	console.log(err.reason)
+        	Session.set('displayMessage', err.reason);
+        }else{
+        	console.log('success');
+        	FlowRouter.go('/');
+        }
+      });
+         return false; 
+      }
 });
