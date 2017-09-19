c6471ec6a9ac922046e76800f7a504daa3ecdb93
likestrike <sesim5351@gmail.com>
2017-09-19 18:45:56 +0900

    2017-09-19 essim moment 적용. 기타 수정.

--- client/templates/views/posts/postitem.js
+++ client/templates/views/posts/postitem.js
@@ -1,9 +1,9 @@
 Template.postItem.helpers({
-	writerthumb: function() {
-	   return Meteor.users.find({profile: {_id: this.userId}});
-	},
 	writer: function() {
-		return Meteor.users.find({'profile.fullname' : {_id: this.userId}});
+		// return Meteor.users.find({'profile' : {_id: this.userId}});
+		return Meteor.users.findOne(this.userId);
+	},
+	formattedDate: function(){
+		return moment(this.submitted).format("dddd, MMMM Do YYYY, h:mm:ss a");
 	}
-
 });
