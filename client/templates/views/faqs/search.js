f3c6ba0c48b6b06c63ab40306bca331768dba281
SHIM\은성 <sesim5351@gmail.com>
2017-09-25 01:17:38 +0900

    2017-09-25 essim home만들기 profile에서 위치 이미지 깨지는 것 수정. 기타 ui정리 등등. 해야할 일 ----------------------------------------------------------------------- 블라인드 게시판 만들기 User list 검색 방안, 조직도 적용 방안 FAQ list easy search collapse 푸는 방안 (key event 받아서 value 확인 후 div 클래스 수정 하는 방법 해보기) Summernote의 이미지 base64를 image 파일로 만들기. -------------------------------------------------------------------------------------------

--- client/templates/views/faqs/search.js
+++ client/templates/views/faqs/search.js
@@ -1,6 +1,6 @@
 Template.search.helpers({
 	inputAttributes: function () {
-		return { 'class': 'easy-search-input', 'placeholder': 'Start searching...' };
+		return { 'class': 'form-control', 'placeholder': '검색하려는 단어를 입력하세요. ' };
 	},
 	index: function() {
 		return FaqsIndex;
