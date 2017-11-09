7dee5215eb2d98df6eb10da85e81094128a3656e
likestrike <sesim5351@gmail.com>
2017-11-09 17:21:31 +0900

    2017-11-09 essim ostrio:files 적용. 모달에 적용. 멀티 이미지 업로드, 이미지 박스, 삭제, 로그 삭제---> 로그 파일을 따로 저장하는 방법으로. 기타 등등 처리 방안 모색. 1차 업로드 확인 완료.

---
+++ client/templates/views/fileupload/upload.js
@@ -0,0 +1,51 @@
+import Images from '/collections/images.js';
+
+Template.uploadedFiles.helpers({
+  uploadedFiles: function () {
+    return Images.find();
+  }
+});
+
+Template.uploadForm.onCreated(function () {
+  this.currentUpload = new ReactiveVar(false);
+});
+
+Template.uploadForm.helpers({
+  currentUpload: function () {
+    return Template.instance().currentUpload.get();
+  }
+});
+
+Template.uploadForm.events({
+  'change #fileInput': function (e, template) {
+    if (e.currentTarget.files && e.currentTarget.files[0]) {
+      // We upload only one file, in case
+      // there was multiple files selected
+      var file = e.currentTarget.files[0];
+      if (file) {
+        var uploadInstance = Images.insert({
+          file: file,
+          streams: 'dynamic',
+          chunkSize: 'dynamic'
+        }, false);
+
+        uploadInstance.on('start', function() {
+          template.currentUpload.set(this);
+        });
+
+        uploadInstance.on('end', function(error, fileObj) {
+          if (error) {
+            // window.alert('Error during upload: ' + error.reason);
+            console.log('Error during upload: ' + error.reason);
+          } else {
+            // window.alert('File "' + fileObj.name + '" successfully uploaded');
+            console.log('File "' + fileObj.name + '" successfully uploaded');
+          }
+          template.currentUpload.set(false);
+        });
+
+        uploadInstance.start();
+      }
+    }
+  }
+});
