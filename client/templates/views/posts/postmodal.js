1898fcda26f817a6eb7d71a26fdce54dc719d72e
likestrike <sesim5351@gmail.com>
2017-11-27 18:59:06 +0900

    2017-11-27 essim 커밋 ogTAg 가져오기 테스트 Url 추출하는 게 안됨

--- client/templates/views/posts/postmodal.js
+++ client/templates/views/posts/postmodal.js
@@ -1,11 +1,23 @@
+
 // sesstion post note를 줘서 div에 복사하는 방식. textarea의 스크롤이나 여려 이벤트를 처리할게 많아서 일단 보류.
-// Template.postModal.helpers({
-// 	postNote: function () {
-// 		var postNote = Session.get("postNote");
-// 		//do processing
-// 		if(postNote!='')return postNote;
-// 	}
-// });
+if(Meteor.isClient){
+	Session.set('metas', '');
+	function getURL = function(text){
+		var urlRegex = /(http(s)?:\/\/[^ \n>]*)|\"(http(s)?:\/\/+)+[^"]*\"/gm; 
+		return text.replace(urlRegex, function(url) {
+	    	var html = url.replace(/\"/g, "");
+	    	return html;
+	    })
+	}
+}
+Template.postModal.helpers({
+	// postNote: function () {
+	// 	var postNote = Session.get("postNote");
+	// 	//do processing
+	// 	if(postNote!='')return postNote;
+	// }
+	metas() { return Session.get('metas'); },
+});
 
 Template.postModal.events({
 	'keydown #post_context': function (e, t) {
@@ -14,6 +26,24 @@ Template.postModal.events({
 		if($('#submit').hasClass('disabled')){
 			$('#submit').removeClass('disabled');
 		}
+	},
+	'keyup #post_context' : function(e, t){
+		if (e.keyCode !== 13) return;
+		var test = $('#post_context').html();
+		var test = $.getURL(test);
+		console.log(test);
+		const url = $('#post_context').html();
+		console.log('extract', url);
+		Session.set('metas', 'Extracting ' + url + '...');
+		extractMeta(url, (err, res) => {
+			if (err) {
+				console.error('err while extracting metas', err);
+				Session.set('metas', 'Error: ' + err);
+			} else {
+				console.log(JSON.stringify(res, null, '  '));
+				Session.set('metas', JSON.stringify(res, null, '  '));
+			}
+		});
 	},
 	'click #submit' : function (e, t) {
 		var post = {
