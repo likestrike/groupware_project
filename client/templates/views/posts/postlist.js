336e5127efe1bc4177dca4a3434d0b2d3318bb92
likestrike <sesim5351@gmail.com>
2017-11-30 19:38:07 +0900

    2017-11-30 essim 3단 레이아웃 완성~!!!!

--- client/templates/views/posts/postlist.js
+++ client/templates/views/posts/postlist.js
@@ -1,14 +1,22 @@
-
+import { Blaze } from 'meteor/blaze';
+var targetIndex = 0;
 Template.postlist.helpers({
 	posts: function() {
-		return Posts.find({},{sort : {submitted : -1}});
+		return Posts.find({}, {sort: {submitted: -1}}).map(function(v, i) {
+	      	v.index = i;
+			return v;
+	    });
+	}, 
+	seperate: function(){
+		var index = this.index;
+		var result = (index%3);
+		targetIndex++;
+		if(result == 0){
+			targetIndex = 0;
+		}
+		Blaze.renderWithData(Template.postItem, {post : this}, $(".col-md-4")[targetIndex])
 	}
 });
-Template.postlist.onRendered(function (){
-	var postData = this.data.posts;
-	
-	console.log(postData);
-})
 Template.postlist.events({
 	'click #post-modal': function (e, t) {
 		e.preventDefault()
